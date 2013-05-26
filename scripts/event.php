ad9e832316de220b894c0049a0dc86cbd2cfedc5
arctarus44 <arthur.dewarumez@gmail.com>
2013-05-27 00:40:09 +0200

    J'ai renommer le fichier index.html en index.php pour pouvoir ajouter mon script event.php. J'ai l'affichage du lien pour le calendrier dans le script pour n'avoir à faire qu'une modification à faire en cas de changement de fichier ics. J'ai ajouter dans le pied de page une réfernce à ics-parser.

--- scripts/event.php
+++ scripts/event.php
@@ -1,47 +1,51 @@
 <?php
 	require 'class.iCalReader.php';
-	$fichier_source = "https://www.google.com/calendar/ical/jq99l7s73opl0ok7b9ieuqhrj4%40group.calendar.google.com/public/basic.ics";
-	$NB_EVENT_AFF = 3; //Nombre d'évènement à afficher par défaut.
-	$pb_fichier = false;//En cas de problème d'ouverture du fichier, on passe à false
 
-	try{
-		$ical = new ical($fichier_source);
-	}
-	catch(Exception $e){
-		$pb_fichier = true;;
-	}
-	if($pb_fichier){//Erreur lors de l'ouverture du fichier
-		echo "<p class=\"error\"> Ooops, l'agenda n'est pas disponible pour le moment. Appuies sur F5 pour voir.</p>";
-	}
-	else{//On récupére les données
-		/*========== Récupération de tous les événements ==========*/
-		if($ical->event_count === 0){//Pas d'événements de prévu
-			echo "<ul>Évènements\n\t<li>Rien de prévue pour le moment. Repasser nous voir pour vous tenir au courant</li>\n</ul>";
+
+	function show_event(){
+		setlocale (LC_TIME, 'fr_FR.utf8','fra'); //Pour avoir les locales en fr
+		$fichier_source = "https://www.google.com/calendar/ical/jq99l7s73opl0ok7b9ieuqhrj4%40group.calendar.google.com/public/basic.ics";
+		$NB_EVENT_AFF = 3; //Nombre d'évènement à afficher par défaut.
+		$pb_fichier = false;//En cas de problème d'ouverture du fichier, on passe à false
+		try{
+			$ical = new ical($fichier_source);
 		}
-		else{
-			$ical_event = $ical->sortEventsWithOrder($ical->events(), SORT_ASC);//On récupère les events trié
-			/*========== Détermination des prochains événements à venir ==========*/
-			$date_courante = intval(date("Ymd"));
-			$it_event = 0;//Pointera au prochain événement à venir à la fin de la boucle
-			while(intval(substr($ical_event[$it_event]['DTSTART'],0, 8) < $date_courante)
-					&& $it_event < count($ical_event)){
-				$it_event++;
+		catch(Exception $e){
+			$pb_fichier = true;;
+		}
+		if($pb_fichier){//Erreur lors de l'ouverture du fichier
+			echo "<p class=\"error\"> Ooops, l'agenda n'est pas disponible pour le moment. Appuies sur F5 pour voir.</p>";
+		}
+		else{//On récupére les données
+			/*========== Récupération de tous les événements ==========*/
+			if($ical->event_count === 0){//Pas d'événements de prévu
+				echo "<ul>Évènements\n\t<li>Rien de prévue pour le moment. Repasser nous voir pour vous tenir au courant</li>\n</ul>";
 			}
-			/*========== Si le nombre d'événements est inférieur à NB_EVENT_AFF ==========*/
-			if(count($ical_event) - $it_event < 3){
-				//On utilise la taille du tableau car le tableau est trié.
-				$NB_EVENT_AFF = count($ical_event) - $it_event;
+			else{
+				$ical_event = $ical->sortEventsWithOrder($ical->events(), SORT_ASC);//On récupère les events trié
+				/*========== Détermination des prochains événements à venir ==========*/
+				$date_courante = intval(date("Ymd"));
+				$it_event = 0;//Pointera au prochain événement à venir à la fin de la boucle
+				while(intval(substr($ical_event[$it_event]['DTSTART'],0, 8) < $date_courante)
+						&& $it_event < count($ical_event)){
+					$it_event++;
+				}
+				/*========== Si le nombre d'événements est inférieur à NB_EVENT_AFF ==========*/
+				if(count($ical_event) - $it_event < 3){
+					//On utilise la taille du tableau car le tableau est trié.
+					$NB_EVENT_AFF = count($ical_event) - $it_event;
+				}
+				/*========== On affiche les prochains événement ==========*/
+				for($i = 0 ; $i < $NB_EVENT_AFF; $i++){
+						echo "\t<ul>".$ical_event[$i+$it_event]['SUMMARY']."\n\t\t";
+						echo "<li>".strftime("%A %e %G",$ical->iCalDateToUnixTimestamp($ical_event[$i+$it_event]['DTSTART']))."</li>\n\t\t";
+						echo "<li>".$ical_event[$i+$it_event]['LOCATION']."</li>\n\n\t\t";
+						echo "<li class=\"Description\">".$ical_event[$i+$it_event]['DESCRIPTION']."</li>\n\t";
+						echo "</ul>\n";
+				}
+				echo "</ul>";
+				echo "<p>Pour toujours êtres à jour, abonnez-vous à notre <a href=\"".$fichier_source."\">calendrier</a></p>";
 			}
-			/*========== On affiche les prochains événement ==========*/
-			echo "<ul>Évènements\n\t";
-			for($i = 0 ; $i < $NB_EVENT_AFF; $i++){
-					echo "\t<ul>".$ical_event[$i+$it_event]['SUMMARY']."\n\t\t";
-					echo "<li>".date("D j F",$ical->iCalDateToUnixTimestamp($ical_event[$i+$it_event]['DTSTART']))."</li>\n\t\t";
-					echo "<li>".$ical_event[$i+$it_event]['LOCATION']."</li>\n\n\t\t";
-					echo "<li class=\"Description\">".$ical_event[$i+$it_event]['DESCRIPTION']."</li>\n\t";
-					echo "</ul>\n";
-			}
-			echo "</ul>";
 		}
 	}
 ?>
